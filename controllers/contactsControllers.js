9ecb88acfc70b9b39227101058ca6887f3aecd26
Vladyslav Salenik <30357798+doranvlad@users.noreply.github.com>
2024-06-01 17:41:20 +0300

    98% logic

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,94 @@
-import contactsService from "../services/contactsServices.js";
+import {
+  addContact,
+  getContactById,
+  listContacts,
+  removeContact,
+  updateContactById,
+} from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const result = await listContacts();
 
-export const getOneContact = (req, res) => {};
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const result = await getContactById(req.params.id);
 
-export const createContact = (req, res) => {};
+    if (!result) {
+      throw HttpError(404, `Not found`);
+    }
 
-export const updateContact = (req, res) => {};
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const result = await removeContact(id);
+
+    if (!result) {
+      throw HttpError(404, `Not found`);
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createContact = async (req, res, next) => {
+  try {
+    const { error } = createContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+
+    const result = await addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  try {
+    const { error } = updateContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(404, `Not found`);
+    }
+
+    const { id } = req.params;
+    const result = await updateContactById(id, req.body);
+
+    if (!result) {
+      throw HttpError(404);
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default {
+  getAllContacts,
+  getOneContact,
+  deleteContact,
+  createContact,
+  updateContact,
+};
