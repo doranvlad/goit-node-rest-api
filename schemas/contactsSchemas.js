0d483ea015d4f89d81095998ffdd057969ee15c3
Vladyslav Salenik <30357798+doranvlad@users.noreply.github.com>
2024-06-11 21:26:48 +0300

    Update contactsSchemas.js

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -4,7 +4,7 @@ export const createContactSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
-  favorive: Joi.boolean(),
+  favorite: Joi.boolean(),
 });
 
 export const updateContactSchema = Joi.object({
